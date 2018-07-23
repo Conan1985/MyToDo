18a061b2af9037b58878f224641ee487bb238eeb
Conan1985 <Conan1985@users.noreply.github.com>
2018-07-23 17:27:36 -0400

    Add 'delete' to lists

---
+++ MyToDo/MyToDo.swift
@@ -0,0 +1,30 @@
+//
+//  MyToDo.swift
+//  MyToDo
+//
+//  Created by Youwei Lu on 7/15/18.
+//  Copyright © 2018 Youwei Lu. All rights reserved.
+//
+
+import Foundation
+class ToDoItem {
+    var title: String
+    var done: Bool
+    
+    public init(title: String) {
+        self.title = title
+        self.done = false
+    }
+    
+}
+
+extension ToDoItem {
+    public class func getMockData() -> [ToDoItem] {
+        return [
+            ToDoItem(title: "Milk"),
+            ToDoItem(title: "Chocolate"),
+            ToDoItem(title: "Light bulb"),
+            ToDoItem(title: "Dog food")
+        ]
+    }
+}
